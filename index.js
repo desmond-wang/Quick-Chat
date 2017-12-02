bed2e662d3dbd66b4f3de2d6d3c5d09b84bc92ab
Daniel Z <danielzheng1997@gmail.com>
2017-12-02 17:38:21 -0500

    api

--- index.js
+++ index.js
@@ -2,30 +2,9 @@ const express = require('express');
 const app = express();
 const QuickBooks = require('node-quickbooks')
 const qbOAuth = require('./quickbookOAuth.json')
+let gallery_tmpo = require('./gallery.json')
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    const rs = [];
-    rs.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
-    res.send(rs);
-});
-
-app.get('/item/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const item = qbo.getItem(id, (error, item) => console.log(item))
-    console.log(item);
-    const rs = item;//getItem(item);
-    rs.push({});
-    res.send(rs);
-});
-
-function getItem(item) {
-    
-}
-    
-app.listen(PORT, () => console.log(`Chatfuel Bot-Server listening on port ${ PORT }`));
- 
 const consumerKey = 'Q04Nh3GPuQZtJah8cPwymbrVWaiZ17cw4d4PpdmZQAPl7Hu7DB';
 const consumerSecret = 'JSij4Gw4aZNMZlAjt5VT8Vi3tZRkd3lHLBmCsmJL';
 const realmId = 123145932193644;
@@ -44,5 +23,54 @@ var qbo = new QuickBooks(consumerKey,
     oauthversion, 
     qbOAuth.refresh_token)
 
+
+app.get('/', (req, res) => {
+    const rs = [];
+    rs.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    res.send(rs);
+});
+
+app.get('/item/:id', (req, res) => {
+    const id = req.params.id;
+    qbo.getItem(id, function(error, item){
+	const rs = [];
+	rs.push(item);
+	res.send(rs);
+    })
+});
+
+app.get('/item', (req, res) => {
+    const category = req.query.category;
+    let query = [
+	{fetchAll: true}, 
+	{Type: 'Inventory'}, 
+	{field: 'FullyQualifiedName', value: `${category}%`, operator: 'LIKE'}
+    ];
+    qbo.findItems(query, function(error, item){
+	const rs = [];
+	const items = item.QueryResponse.Item;
+	for (i = 0; i < items.length; i++) {
+	    block = {
+		"title": items[i].Name,
+		"image_url":"https://rockets.chatfuel.com/img/shirt.png",
+		"subtitle": items[i].Description,
+		"buttons":[
+		    {
+			"type":"web_url",
+			"url":"https://rockets.chatfuel.com/store/shirt",
+			"title":"View Item"
+		    }
+		]
+	    }
+	    gallery_tmpo.attachment.payload.elements.push(block);
+	}
+	rs.push(gallery_tmpo);
+	res.send(rs);
+    })
+});
+
+
+app.listen(PORT, () => console.log(`Chatfuel Bot-Server listening on port ${ PORT }`));
+ 
 
 
