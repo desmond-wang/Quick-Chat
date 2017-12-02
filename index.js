896bdd1903e713d54cce1470669364978e601907
Daniel Zheng <ml@v1020-wn-168-170.campus-dynamic.uwaterloo.ca>
2017-12-01 21:36:57 -0500

    port

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
-app.listen(3000, function() {
-    console.log('Chatfuel Bot-Server listening on port 3000...');
-});
 
-app.get('/*', function(req, res) {
-    const jsonResponse = [];
-    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
-    res.send(jsonResponse);
-});
+
+app
+    // .use(express.static(path.join(__dirname, 'public')))
+    // .set('views', path.join(__dirname, 'views'))
+    // .set('view engine', 'ejs')
+    .get('/*', (req, res) =>
+	const jsonResponse = [];
+	jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+	res.send(jsonResponse););
+    .listen(PORT, () => console.log('Chatfuel Bot-Server listening on port ${ PORT }'));
