2f9324f0c0adee2a26123e94319460b6a3c6787e
Daniel Z <danielzheng1997@gmail.com>
2017-12-01 22:10:08 -0500

    set up

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const PORT = process.env.PORT || 5000
 
 
 
-app
-    // .use(express.static(path.join(__dirname, 'public')))
-    // .set('views', path.join(__dirname, 'views'))
-    // .set('view engine', 'ejs')
-    .get('/*', (req, res) =>
+app.get('/', (req, res) => {
 	const jsonResponse = [];
 	jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
-	res.send(jsonResponse););
-    .listen(PORT, () => console.log('Chatfuel Bot-Server listening on port ${ PORT }'));
+	res.send(jsonResponse);
+    })
+    .listen(PORT, () => console.log(`Chatfuel Bot-Server listening on port ${ PORT }`));
